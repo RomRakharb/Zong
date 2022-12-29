65a7c8040667a4bdb6ecb09b8b4d2f4b3a8eebc0
RomRakharb <rom.rakharb@outlook.com>
2022-12-29 14:32:29 +0700

    first time

---
+++ main.py
@@ -0,0 +1,156 @@
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.pagesizes import A4, landscape
+import pandas
+import os
+import calendar
+import time
+import pathlib
+import shutil
+
+def clear():
+    for f in os.listdir('storage'):
+        if not f.endswith(".pdf"):
+            continue
+        try:
+            os.remove(os.path.join('storage', f))
+        except:
+            pass
+def reset():
+    df = pandas.read_excel('zong.xlsx')
+    name_list = []
+    i = 0
+    while True:
+        try:
+            name_list.append(str(df.loc[i][0]))
+            i += 1
+        except:
+            break
+    return name_list
+
+
+def saveItem(index, list):
+    df = pandas.read_excel('zong.xlsx')
+    df.loc[index] = list
+    df.to_excel('zong.xlsx', index=False, header=['topic', 'line1', 'line2', 'line3', 'line4', 'line5'])
+
+
+def deleteAction(index):
+    df = pandas.read_excel('zong.xlsx')
+    df.drop(index, axis=0, inplace=True)
+    df.to_excel('zong.xlsx', index=False, header=['topic', 'line1', 'line2', 'line3', 'line4', 'line5'])
+
+
+def selectedItem(name):
+    name_list = reset()
+    df = pandas.read_excel('zong.xlsx')
+    receiver = df.loc[name_list.index(name)]
+    return receiver
+
+
+def printEnvelope(des, notSend, name):
+    name_list = reset()
+    df = pandas.read_excel('zong.xlsx')
+    sender = df.loc[0]
+    receiver = df.loc[name_list.index(des)]
+    pdfmetrics.registerFont(TTFont('TH Sarabun New', 'THSarabunNew.ttf'))
+    current_GMT = time.gmtime()
+    ts = calendar.timegm(current_GMT)
+    filename = str(name_list.index(des)) + '-' + str(ts) + '.pdf'
+    c = canvas.Canvas(filename)
+    c.setFont('TH Sarabun New', 16)
+    c.setPageSize((683.1496063, 297.63779528))
+    ySender = 200
+    yReceiver = 120
+    xSender = 70
+    xPreReceiver = 260
+    xReceiver = 310
+    xStamp = 575
+    yStamp = 240
+    xRect = 500
+    yRect = 175
+    xNotsend = 500
+    yNotsend = 50
+    for i in range(6):
+        try:
+            c.drawString(xSender, ySender, sender[i + 1])
+            ySender -= 20
+        except:
+            break
+    c.drawString(xPreReceiver, yReceiver, 'เรียน')
+    for i in range(6):
+        try:
+            c.drawString(xReceiver, yReceiver, receiver[i + 1])
+            yReceiver -= 20
+        except:
+            break
+    if notSend is False:
+        c.rect(xRect, yRect, 150, 100)
+        with open('zong.txt', encoding='utf8') as f:
+            lines = f.readlines()
+            for line in lines:
+                c.drawCentredString(xStamp, yStamp, line.replace('\n', ''))
+                yStamp -= 20
+    else:
+        c.drawString(xNotsend, yNotsend, '( ' + name + ' )')
+    c.save()
+    shutil.move(str(pathlib.Path().resolve()) + '\\' + filename,
+                str(pathlib.Path().resolve()) + "\\storage\\" + filename)
+    os.startfile(str(pathlib.Path().resolve()) + "\\storage\\" + filename)
+
+
+def printA4(des, notSend, name):
+    name_list = reset()
+    df = pandas.read_excel('zong.xlsx')
+    sender = df.loc[0]
+    receiver = df.loc[name_list.index(des)]
+    pdfmetrics.registerFont(TTFont('TH Sarabun New', 'THSarabunNew.ttf'))
+    current_GMT = time.gmtime()
+    ts = calendar.timegm(current_GMT)
+    filename = str(name_list.index(des)) + '-' + str(ts) + '.pdf'
+    c = canvas.Canvas(filename)
+    c.setFont('TH Sarabun New', 20)
+    c.setPageSize(landscape(A4))
+    x_max = 842
+    y_max = 595
+    xSender = 50
+    ySender = y_max-150
+    xPreReceiver = x_max/2 - 100
+    xReceiver = x_max/2 - 50
+    yReceiver = y_max/2
+    xStamp = x_max - 150
+    yStamp = y_max - 80
+    xRect = x_max - 250
+    yRect = y_max - 150
+    xNotsend = x_max/2
+    yNotsend = y_max/4
+    spacing = 25
+    c.rect(20, 20, 802, 555)
+    c.drawImage('garuda.png', 50, y_max-175, 50, mask='auto', preserveAspectRatio=True)
+    for i in range(6):
+        try:
+            c.drawString(xSender, ySender, sender[i + 1])
+            ySender -= spacing
+        except:
+            break
+    c.drawString(xPreReceiver, yReceiver, 'เรียน')
+    for i in range(6):
+        try:
+            c.drawString(xReceiver, yReceiver, receiver[i + 1])
+            yReceiver -= spacing
+        except:
+            break
+    if notSend is False:
+        c.rect(xRect, yRect, 200, 100)
+        with open('zong.txt', encoding='utf8') as f:
+            lines = f.readlines()
+            for line in lines:
+                c.drawCentredString(xStamp, yStamp, line.replace('\n', ''))
+                yStamp -= spacing
+    else:
+        c.drawCentredString(xNotsend, yNotsend, '( ' + name + ' )')
+    c.save()
+    shutil.move(str(pathlib.Path().resolve()) + '\\' + filename,
+                str(pathlib.Path().resolve()) + "\\storage\\" + filename)
+    os.startfile(str(pathlib.Path().resolve()) + "\\storage\\" + filename)
---
+++ zong.py
@@ -0,0 +1,377 @@
+import os
+
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QMessageBox, QInputDialog
+from main import reset, selectedItem, printEnvelope, printA4, saveItem, deleteAction, clear
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1000, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(50, 25, 900, 400))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setHorizontalSpacing(10)
+        self.gridLayout.setVerticalSpacing(4)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.printButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.printButton.setFont(font)
+        self.printButton.setObjectName("printButton")
+        self.verticalLayout.addWidget(self.printButton)
+        self.resetButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.resetButton.setFont(font)
+        self.resetButton.setObjectName("resetButton")
+        self.verticalLayout.addWidget(self.resetButton)
+        self.editButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.editButton.setFont(font)
+        self.editButton.setObjectName("editButton")
+        self.verticalLayout.addWidget(self.editButton)
+
+        self.deleteButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.deleteButton.setFont(font)
+        self.deleteButton.setObjectName("deleteButton")
+        self.verticalLayout.addWidget(self.deleteButton)
+
+        self.checkBox = QtWidgets.QCheckBox(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.checkBox.setFont(font)
+        self.checkBox.setObjectName("checkBox")
+        self.verticalLayout.addWidget(self.checkBox)
+        self.checkBox_2 = QtWidgets.QCheckBox(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.checkBox_2.setFont(font)
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.verticalLayout.addWidget(self.checkBox_2)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 2, 1, 1)
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setHorizontalSpacing(4)
+        self.formLayout.setVerticalSpacing(10)
+        self.formLayout.setObjectName("formLayout")
+        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label_2)
+
+        # name combobox
+        self.topic_comboBox = QtWidgets.QComboBox(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.topic_comboBox.setFont(font)
+        self.topic_comboBox.setEditable(True)
+        self.topic_comboBox.setDuplicatesEnabled(False)
+        self.topic_comboBox.completer().setCompletionMode(QtWidgets.QCompleter.CompletionMode.PopupCompletion)
+        self.topic_comboBox.completer().setFilterMode(QtCore.Qt.MatchFlag.MatchContains)
+        self.topic_comboBox.setObjectName("topic_comboBox")
+        self.topic_comboBox.addItems(reset())
+        self.topic_comboBox.setCurrentText('')
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.ItemRole.FieldRole, self.topic_comboBox)
+
+        self.line1 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.line1.setFont(font)
+        self.line1.setObjectName("line1")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.ItemRole.FieldRole, self.line1)
+        self.line2 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.line2.setFont(font)
+        self.line2.setObjectName("line2")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.ItemRole.FieldRole, self.line2)
+        self.line3 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.line3.setFont(font)
+        self.line3.setObjectName("line3")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.ItemRole.FieldRole, self.line3)
+        self.line4 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.line4.setFont(font)
+        self.line4.setObjectName("line4")
+        self.formLayout.setWidget(5, QtWidgets.QFormLayout.ItemRole.FieldRole, self.line4)
+        self.line5 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.line5.setFont(font)
+        self.line5.setObjectName("line5")
+        self.formLayout.setWidget(6, QtWidgets.QFormLayout.ItemRole.FieldRole, self.line5)
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.label.setFont(font)
+        self.label.setFrameShape(QtWidgets.QFrame.Shape.NoFrame)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label)
+        self.gridLayout.addLayout(self.formLayout, 0, 0, 1, 1)
+
+        self.listWidget = QtWidgets.QListWidget(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.listWidget.setFont(font)
+        self.listWidget.setObjectName("listWidget")
+        self.listWidget.addItems(reset())
+        self.gridLayout.addWidget(self.listWidget, 0, 1, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 798, 30))
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.menubar.setFont(font)
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.menuFile.setFont(font)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionOpen_Text_File = QtGui.QAction(MainWindow)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.actionOpen_Text_File.setFont(font)
+        self.actionOpen_Text_File.setObjectName("actionOpen_Text_File")
+        self.actionOpen_Excel_File = QtGui.QAction(MainWindow)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        self.actionOpen_Excel_File.setFont(font)
+        self.actionOpen_Excel_File.setObjectName("actionOpen_Excel_File")
+        self.menuFile.addAction(self.actionOpen_Text_File)
+        self.menuFile.addAction(self.actionOpen_Excel_File)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.resetAll()
+
+        self.actionOpen_Text_File.triggered.connect(lambda: os.startfile('zong.txt'))
+        self.actionOpen_Excel_File.triggered.connect(lambda: os.startfile('zong.xlsx'))
+        self.topic_comboBox.activated.connect(lambda: self.selectItem(name=self.topic_comboBox.currentText()))
+        self.printButton.clicked.connect(self.printAction)
+        self.resetButton.clicked.connect(self.resetAll)
+        self.editButton.clicked.connect(self.editItem)
+        self.deleteButton.clicked.connect(self.deleteItem)
+        self.listWidget.itemClicked.connect(lambda: self.selectItem(index=self.listWidget.currentRow()))
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "ซอง"))
+        self.printButton.setText(_translate("MainWindow", "Print"))
+        self.resetButton.setText(_translate("MainWindow", "Reset"))
+        self.editButton.setText(_translate("MainWindow", "Edit"))
+        self.deleteButton.setText(_translate("MainWindow", "Delete"))
+        self.checkBox.setText(_translate("MainWindow", "A4"))
+        self.checkBox_2.setText(_translate("MainWindow", "รับเอง"))
+        self.label_2.setText(_translate("MainWindow", "เรียน"))
+        self.label.setText(_translate("MainWindow", "หัวข้อ"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.actionOpen_Text_File.setText(_translate("MainWindow", "เลขอนุญาต"))
+        self.actionOpen_Excel_File.setText(_translate("MainWindow", "ไฟล์ Exel"))
+
+    def resetAll(self):
+        self.topic_comboBox.clear()
+        self.topic_comboBox.addItems(reset())
+        self.topic_comboBox.setCurrentText('')
+        self.topic_comboBox.setDisabled(False)
+        self.listWidget.clear()
+        self.listWidget.addItems(reset())
+        self.line1.clear()
+        self.line1.setDisabled(True)
+        self.line2.clear()
+        self.line2.setDisabled(True)
+        self.line3.clear()
+        self.line3.setDisabled(True)
+        self.line4.clear()
+        self.line4.setDisabled(True)
+        self.line5.clear()
+        self.line5.setDisabled(True)
+        self.printButton.setDisabled(False)
+        self.deleteButton.setDisabled(False)
+        self.editButton.setText('Edit')
+
+    def printAction(self):
+        des_list = reset()
+        try:
+            currentIndex = des_list.index(self.topic_comboBox.currentText())
+            if self.topic_comboBox.currentText() == 'เพิ่มที่อยู่':
+                return None
+        except:
+            return None
+        name = None
+        if self.checkBox_2.isChecked() is True:
+            name = self.printBox()
+            if name is None:
+                return None
+        if self.checkBox.isChecked():
+            printA4(self.topic_comboBox.currentText(), self.checkBox_2.isChecked(), name)
+        else:
+            printEnvelope(self.topic_comboBox.currentText(), self.checkBox_2.isChecked(), name)
+
+    def editItem(self):
+        newName = []
+        nameList = reset()
+        if self.editButton.text() == 'Edit':
+            try:
+                currentIndex = nameList.index(self.topic_comboBox.currentText())
+            except:
+                return None
+            self.printButton.setDisabled(True)
+            self.deleteButton.setDisabled(True)
+            if self.topic_comboBox.currentText() != 'เพิ่มที่อยู่':
+                self.topic_comboBox.setDisabled(True)
+            self.line1.setDisabled(False)
+            self.line2.setDisabled(False)
+            self.line3.setDisabled(False)
+            self.line4.setDisabled(False)
+            self.line5.setDisabled(False)
+            self.editButton.setText('Save')
+        else:
+            newName.append(self.topic_comboBox.currentText())
+            newName.append(self.line1.text())
+            newName.append(self.line2.text())
+            newName.append(self.line3.text())
+            newName.append(self.line4.text())
+            newName.append(self.line5.text())
+            if self.topic_comboBox.currentText() != 'เพิ่มที่อยู่':
+                try:
+                    currentIndex = nameList.index(self.topic_comboBox.currentText())
+                    saveItem(currentIndex, newName)
+                except Exception as e:
+                    saveItem(-1, newName)
+            self.resetAll()
+
+    def deleteItem(self):
+        nameList = reset()
+        try:
+            currentIndex = nameList.index(self.topic_comboBox.currentText())
+            if self.topic_comboBox.currentText() == 'เพิ่มที่อยู่' or self.topic_comboBox.currentText() == nameList[0]:
+                return None
+        except:
+            return None
+        if self.deleteBox():
+            deleteAction(currentIndex)
+            self.resetAll()
+
+    def selectItem(self, index=None, name=None):
+        # self.resetAll()
+        self.listWidget.clear()
+        self.listWidget.addItems(reset())
+        self.line1.clear()
+        self.line1.setDisabled(True)
+        self.line2.clear()
+        self.line2.setDisabled(True)
+        self.line3.clear()
+        self.line3.setDisabled(True)
+        self.line4.clear()
+        self.line4.setDisabled(True)
+        self.line5.clear()
+        self.line5.setDisabled(True)
+        self.editButton.setText('Edit')
+
+        name_list = reset()
+        try:
+            if index is not None:
+                data_list = selectedItem(name_list[index])
+            if name is not None:
+                data_list = selectedItem(name)
+            try:
+                self.topic_comboBox.setCurrentText(data_list[0])
+                self.line1.setText(data_list[1])
+                self.line2.setText(data_list[2])
+                self.line3.setText(data_list[3])
+                self.line4.setText(data_list[4])
+                self.line5.setText(data_list[5])
+            except:
+                pass
+        except:
+            self.line1.setText('')
+            self.line2.setText('')
+            self.line3.setText('')
+            self.line4.setText('')
+            self.line5.setText('')
+
+    def printBox(self):
+        msg = QInputDialog()
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        msg.setFont(font)
+        msg.setWindowTitle('ชื่อผู้รับ')
+        # msg.labelText('ชื่อผู้รับ')
+        x = msg.exec()
+        if x:
+            return msg.textValue()
+        else:
+            return None
+
+    def deleteBox(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Icon.Warning)
+        font = QtGui.QFont()
+        font.setFamily("TH Sarabun New")
+        font.setPointSize(18)
+        msg.setFont(font)
+        msg.setWindowTitle('Delete')
+        msg.setText('Delete "' + self.topic_comboBox.currentText() + '"')
+        msg.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+
+        x = msg.exec()
+
+        if x == QMessageBox.StandardButton.Yes:
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    import sys
+
+    clear()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
